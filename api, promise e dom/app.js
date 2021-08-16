91a2347694a7e5262daf558bb95c7bd269184769
Lucas Mendonça <lucas21rio@gmail.com>
2021-08-16 12:55:35 -0300

    Usando async await e css melhorado

--- api, promise e dom/app.js
+++ api, promise e dom/app.js
@@ -1,20 +1,40 @@
-// const fetch = require('node-fetch');
 const main = document.querySelector('.movies')
 
+async function getMovie() {
+    const fetchedMovies = await fetch('https://api.themoviedb.org/3/discover/movie?with_genres=27&language=pt-BR&api_key=b2568761987165acaa449c0511b534ea')
+    try {
+        const movies = await fetchedMovies.json()
+        movies.results.forEach(filme => {
+            main.innerHTML += `
+            <div class="moviesUnique">
+            <div class="poster">
+            <a target="_blank" href="https://www.youtube.com/results?search_query=${filme.title} trailer"><img src="https://image.tmdb.org/t/p/w300${filme.poster_path}"></a>
+            </div>
+            <h2 class="descricao">${filme.title}</h2>
+            </div>
+            `
+        })
+    } catch(e) {
+        console.log(e)
+    }
+}
+getMovie()
 
-fetch('https://api.themoviedb.org/3/discover/movie?with_genres=27&language=pt-BR&api_key=b2568761987165acaa449c0511b534ea')
-.then(response => response.json())
-    .then(jsonObtido => jsonObtido.results.forEach(filme => {
-        main.innerHTML += `
-        <div class="moviesUnique">
-        <div class="poster">
-        <a target="_blank" href="https://www.youtube.com/results?search_query=${filme.title} trailer"><img src="https://image.tmdb.org/t/p/w300${filme.poster_path}"></a>
-        </div>
-        <h2 class="descricao">${filme.title}</h2>
-        </div>
-        `
-    }))
-        
+
+// const fetch = require('node-fetch');
+
+// fetch('https://api.themoviedb.org/3/discover/movie?with_genres=27&language=pt-BR&api_key=b2568761987165acaa449c0511b534ea')
+// .then(response => response.json())
+//     .then(jsonObtido => jsonObtido.results.forEach(filme => {
+//         main.innerHTML += `
+//         <div class="moviesUnique">
+//         <div class="poster">
+//         <a target="_blank" href="https://www.youtube.com/results?search_query=${filme.title} trailer"><img src="https://image.tmdb.org/t/p/w300${filme.poster_path}"></a>
+//         </div>
+//         <h2 class="descricao">${filme.title}</h2>
+//         </div>
+//         `
+//     }))
 
 
 
